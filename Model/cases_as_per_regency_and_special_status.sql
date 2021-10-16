064eb48ac14e5f6acc2a2db05bc55d6426326bda
HarshCHhz <33054269+HarshCHhz@users.noreply.github.com>
2021-10-16 19:09:12 +0530

    Update cases_as_per_regency_and_special_status.sql

--- Model/cases_as_per_regency_and_special_status.sql
+++ Model/cases_as_per_regency_and_special_status.sql
@@ -1,6 +1,7 @@
 select a.Date,b.Location,
 case when b.special_status is null then 'No Special Status' else special_Status end special_status ,c.total_regencies,
-sum(new_cases) daily_new_case,sum(new_deaths) daily_new_deaths,sum(NEW_RECOVERED) daily_new_recovery
+sum(new_cases) daily_new_case,sum(new_deaths) daily_new_deaths,sum(NEW_RECOVERED) daily_new_recovery,sum(total_cases) as total_case_reported,
+sum(total_deaths) as total_death_reported
 from 
 "INTERVIEW_DB"."PLAYGROUND_HARSH_CHOUDHARY"."COVID19_data" a
 left join 
@@ -9,4 +10,4 @@ on a.Location_ISO_Code = b.Location_ISO_Code
 left join 
 "INTERVIEW_DB"."PLAYGROUND_HARSH_CHOUDHARY"."Country_level_details" c
 on a.Location_ISO_Code = c.Location_ISO_Code 
-group by 1,2,3,4;
+group by 1,2,3,4
